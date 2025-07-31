c4cedca8ea6ca20ba11598a7ee96a324f8b48429
soo0711 <k300hys@naver.com>
2025-07-31 23:05:24 +0900

    feat: 탈퇴 30일 지난 사용자 매월 1일마다 삭제 배치 구현

---
+++ src/main/java/com/seeat/server/domain/user/application/batch/batchDeleteUsers.java
@@ -0,0 +1,29 @@
+package com.seeat.server.domain.user.application.batch;
+
+import com.seeat.server.domain.user.domain.entity.User;
+import com.seeat.server.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class batchDeleteUsers {
+
+    private final UserRepository userRepository;
+
+    @Scheduled(cron = "0 0 0 1 * ?") // 매월 1일 0시 실행
+    public void deleteUsersMonthly() {
+
+        // updated 30일 지난것만 지우기
+        LocalDateTime cutoffDate = LocalDateTime.now().minusDays(30);
+        List<User> usersToDelete = userRepository.findAllByIsDeleteTrueAndUpdatedAtBefore(cutoffDate);
+
+        userRepository.deleteAll(usersToDelete);
+    }
+}
--- src/main/java/com/seeat/server/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/seeat/server/domain/user/domain/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.seeat.server.domain.user.domain.entity.User;
 import com.seeat.server.domain.user.domain.entity.UserSocial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByIdAndIsDeleteFalse(Long userId);
+
+    List<User> findAllByIsDeleteTrueAndUpdatedAtBefore(LocalDateTime cutoffDate);
+
 }
