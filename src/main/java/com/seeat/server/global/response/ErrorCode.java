f86277771ba3ed89d75ecbefab6b3d96b0108c1d
soo0711 <k300hys@naver.com>
2025-08-01 23:11:11 +0900

    fix: 충돌 해결 세미콜론 누락 수정

--- src/main/java/com/seeat/server/global/response/ErrorCode.java
+++ src/main/java/com/seeat/server/global/response/ErrorCode.java
@@ -103,7 +103,8 @@ public enum ErrorCode {
     NOT_OWN_USER_REVIEW(3006,HttpStatus.NOT_FOUND,"리뷰 작성자와 유저가 상이합니다."),
     NO_IMAGE_REVIEW(3007, HttpStatus.BAD_REQUEST, "등록할 이미지가 없음에도 추가하고자 합니다."),
     TOO_MANY_IMAGES(3008, HttpStatus.BAD_REQUEST, "등록할 이미지가 5개 초과입니다."),
-      NOT_GET_LANGCHAIN(3009,HttpStatus.NOT_IMPLEMENTED,"랭체인 서버에서 값을 받지 못했습니다.");
+    NOT_GET_LANGCHAIN(3009,HttpStatus.NOT_IMPLEMENTED,"랭체인 서버에서 값을 받지 못했습니다."),
+
     // ========================
     // 4000~4999 : 검색어 관련 에러
     // ========================
