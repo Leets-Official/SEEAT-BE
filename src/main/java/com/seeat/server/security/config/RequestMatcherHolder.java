328df06169bcb317537c6bf5a1f1dbaa4455e2a7
eedo_y <doup2001@naver.com>
2025-08-08 00:14:24 +0900

    !!삭제할 커밋!! -USER

--- src/main/java/com/seeat/server/security/config/RequestMatcherHolder.java
+++ src/main/java/com/seeat/server/security/config/RequestMatcherHolder.java
@@ -42,11 +42,11 @@ public class RequestMatcherHolder {
             new RequestInfo(GET, "/api/v1/hashtag/**", null),
 
             // 마이페이지 관련
-            new RequestInfo(GET, "/api/v1/profile", null),
-            new RequestInfo(PATCH, "/api/v1/profile", null),
-            new RequestInfo(GET, "/api/v1/profile/grades", null),
-            new RequestInfo(GET, "/api/v1/profile/**", null),
-            new RequestInfo(DELETE, "/api/v1/profile/**", null),
+            new RequestInfo(GET, "/api/v1/profile", USER),
+            new RequestInfo(PATCH, "/api/v1/profile", USER),
+            new RequestInfo(GET, "/api/v1/profile/grades", USER),
+            new RequestInfo(GET, "/api/v1/profile/**", USER),
+            new RequestInfo(DELETE, "/api/v1/profile/**", USER),
 
             // 리뷰 관련
             new RequestInfo(POST, "/api/v1/reviews/**", USER),
